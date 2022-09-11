6f252c74a04a9168c0ae561ffef823fdd525c0b6
zalven-official <zalvendayao888@gmail.com>
2022-09-11 14:41:35 +0800

    deploying to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/zalven-portfolio/',
   plugins: [react()],
   test: {
     globals: true,
